9eb459149f35201905ae4657bc6decf4fe2b9a72
casey0808 <casey_c0808@outlook.com>
2020-07-08 20:27:12 +0800

    Updates

---
+++ precache-manifest.d14391b44cb97c778634b1b920a1c698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab461a70d766732d65fe96ebffbc349",
+    "url": "/color-game-react/index.html"
+  },
+  {
+    "revision": "ca869a9c392757a009c4",
+    "url": "/color-game-react/static/css/main.3bacfe64.chunk.css"
+  },
+  {
+    "revision": "735e7a37bc1b18098444",
+    "url": "/color-game-react/static/js/2.14fe324d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-game-react/static/js/2.14fe324d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca869a9c392757a009c4",
+    "url": "/color-game-react/static/js/main.35166575.chunk.js"
+  },
+  {
+    "revision": "a02c9df573f74143b23c",
+    "url": "/color-game-react/static/js/runtime-main.8f5b8680.js"
+  }
+]);
